d359691696f5ef53f19a506ce91c495dc69ce58f
pradellese <cborlenghi@outlook.com>
2021-12-06 19:00:58 +1100

    add menu toggle function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio!")
+
+function menuToggle () {
+    const navClassName = document.querySelector('.nav-toggle').className;
+    if (navClassName === 'nav-toggle') {
+        document.querySelector('.nav-toggle').className = 'nav-toggle responsive'
+    } else {
+        document.querySelector('.nav-toggle').className = 'nav-toggle'
+    }
+}
